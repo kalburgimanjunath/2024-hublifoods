937de9de07ac791807c2112542a889a477400b8c
Manjunath Kalburgi <1643278+kalburgimanjunath@users.noreply.github.com>
2024-01-04 08:02:59 +0000

    Update 3 files

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,9 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useSelector } from "react-redux";
 import { Back, Products } from "../components";
-
+import useRazorpay from "react-razorpay";
+import * as alertify from "alertifyjs";
+import "alertifyjs/build/css/alertify.css";
 export default function Cart() {
   const [sumTotal, setSumTotal] = useState(0);
+  const [Razorpay] = useRazorpay();
+  const handlePayment = useCallback(async () => {
+    const options = {
+      key: "rzp_test_XDZYohVji5WV7J",
+      amount: "1000",
+      currency: "INR",
+      isUPILinkEnabled: true,
+      name: "HubliFoods app",
+      description: "HubliFoods app",
+      entity: "order",
+      id: "order_9A33XWu170gUtm",
+      prefill: {
+        name: "manjunath kalburgi",
+        email: "manjunathkalburgi@example.com",
+        contact: "9986407307",
+      },
+      notes: {
+        address: "HubliFoods Corporate Office",
+      },
+      notifyOptions: {
+        sms: true,
+        email: true,
+      },
+      handler: function (response) {
+        console.log(response.razorpay_payment_id);
+        console.log(response.razorpay_order_id);
+        console.log(response.razorpay_signature);
+      },
+      callbackUrl: "https://example-callback-url.com/",
+      callbackMethod: "get",
+      enableReminder: "true",
+      customOptions: {
+        test: "testing",
+      },
+    };
+    const rzpay = new Razorpay(options);
+
+    rzpay.open();
+    alertify.success("Order placed successfully...");
+  }, [Razorpay]);
+
   const cartItems = useSelector((state) => {
     return state.cart;
   });
@@ -41,7 +84,7 @@ export default function Cart() {
         <span>{sumTotal} Rs.</span>
       </div>
       <div className="mt-4">
-        <button type="button" className="cart-button">
+        <button type="button" className="cart-button" onClick={handlePayment}>
           Secure Checkout
         </button>
       </div>
